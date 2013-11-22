6aae2e3d55e8a305298076e741787ea707883381
Dan McGarry <dmcgarry@pacificpolicy.org>
2013-11-22 11:21:05 +1100

    Upload script looks like it's working

--- db/upload-budget-record.pl
+++ db/upload-budget-record.pl
@@ -4,6 +4,7 @@ use strict;
 use Data::Dumper;
 use Store::CouchDB;
 use File::Basename;
+use JSON;
 
 my $csv_file    = $ARGV[0] or die &usage();
 my $budget_key  = $ARGV[1] or die &usage();
@@ -17,24 +18,31 @@ my %upload_rec = ();
 open CSV, $csv_file or die "Couldn't read file '$csv_file': $!\n";
 my $first = 0;
 
-$upload_rec{_id}  = $budget_key;
-$root->{name} = $budget_name;
+# Initialise the record
+$upload_rec{_id}                = $budget_key;
+$upload_rec{root}->{name}       = $budget_name;
+$upload_rec{level}              = 'Sectors';
+$upload_rec{root}->{data}       = {};
+$upload_rec{categories}         = {};
 
 while (<CSV>){
-  $first++;
-  next if $first == 1;
-
   my ($country, $year, $type, $value, $sector, $dept, $programme, $name, $notes, $change, @etc) = split (/\,/, $_);
-  chomp $change;
+
+  # Check for column headings
+  next if $year =~ /year/i;
+
+  $sector    =~ s!\s+!!g;
+  $dept      =~ s!\s+!!g;
+  $programme =~ s!\s+!!g;
+  $value     =~ s!\s+!!g;
 
   # Top level data
   unless ($sector){
 
-    my $root = $upload_rec{root} || {};
-
+    my $root = $upload_rec{root};
     $root->{data}->{$year}->{$type}  = $value;
-    $root->{data}->{$year}->{change} = $change;
-    $root->{data}=>{$year}->{notes}  .= "\n$notes";
+    $root->{data}->{$year}->{change} = $change ? $change : 0;;
+#    $root->{data}=>{$year}->{notes}  .= "\n$notes";
 
     $upload_rec{root} = $root;
     next;
@@ -43,22 +51,72 @@ while (<CSV>){
   # Sectoral data
   unless ($dept){
 
-    
+    die "Missing sector data from line '$_'!\n" unless (defined $sector && $sector);
+
+    my $categories = $upload_rec{categories};
+    my $sect       = $categories->{$sector} || {};
+
+    $sect->{name} = $name;
+    $sect->{data}->{$year}->{$type}  = $value;
+    $sect->{data}->{$year}->{change} = $change ? $change : 0;
+
+    $categories->{$sector}             = $sect;
+    $upload_rec{categories}            = $categories;
+
     next;
   }
 
   # Departmental data
   unless ($programme){
 
+    die "Missing department data from line '$_'!\n" unless (defined $dept && $dept);
+
+    my $categories = $upload_rec{categories}         || {};
+    my $sect       = $categories->{$sector}          || {};
+    my $department = $sect->{categories}->{$dept}    || {};
+
+    $department->{name} = $name;
+    $department->{data}->{$year}->{$type}  = $value;
+    $department->{data}->{$year}->{change} = $change ? $change : 0;
+
+    $sect->{categories}->{$dept}           = $department;
+    $categories->{$sector}                 = $sect;
+    $upload_rec{categories}                = $categories;
+
     next;
   }
 
   # Programme data
+  die "Missing programme data from line '$_'!\n" unless (defined $programme && $programme);
 
+  my $categories = $upload_rec{categories}                 || {};
+  my $sect       = $categories->{$sector}                  || {};
+  my $department = $sect->{categories}->{$dept}            || {};
+  my $prog       = $department->{categories}->{$programme} || {};
+
+  $prog->{name} = $name;
+  $prog->{data}->{$year}->{$type}         = $value;
+  $prog->{data}->{$year}->{change}        = $change ? $change : 0;;
+
+  $department->{categories}->{$programme} = $prog;
+  $sect->{categories}->{$dept}            = $department;
+  $categories->{$sector}                  = $sect;
+  $upload_rec{categories}                 = $categories;
 
 }
 
-#print Dumper($budget_data_in);
+print to_json(\%upload_rec);
+
+sub get_notes {
+
+  # This is going to be a separate process, in which we pull an arbitrary number of notes
+  # from a separate file based on either the sectoral, departmental or programme key.
+
+  my $key = shift or return "";
+
+  return "Notes go here";
+
+}
 
 sub usage {
   my $command = basename($0);
