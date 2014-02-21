3a17e1ac53dc5c3413e13e14b4d0c8de4d8df54f
Dan McGarry <dmcgarry@pacificpolicy.org>
2014-02-21 14:54:35 +1100

    More one-off charts

--- frontend/app/scripts/controllers/budgets.js
+++ frontend/app/scripts/controllers/budgets.js
@@ -281,29 +281,125 @@ angular.module('pippDataApp.controllers.budgets', ['ui.bootstrap', 'ngAnimate', 
     }]);
 
 
-// One-off controllers
-
+// One-off charts controller
 angular.module('pippDataApp.controllers.one-off-charts', ['ui.bootstrap', 'ngAnimate', 'legendDirectives'])
-    .controller('oneOffChartsCtrl', ['$scope', '$location', '$routeParams', function ($scope, $location, $routeParams) {
+    .controller('oneOffChartsCtrl', ['$scope', '$location', '$routeParams', '$interval', function ($scope, $location, $routeParams, $interval) {
 
+	// Pushed a brewer palette pair into the first two positions.
+	// Otherwise, it's the nvd3 default 20 colour palette
 	$scope.current_palette = 	    
 	    [
 		'#ef8a62', '#67a9cf', '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', 
 		'#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'
 	    ];
 
+/*
+	
+	var vu_revenues = [[2011,12850.6],[2012,13612.4],[2013,14554.2],[2014,15252.7]];
+	var vu_values   = [[2011,13840],[2012,14186.5],[2013,14184.2],[2014,14661.7]];
 
 	$scope.vuRevenueExpenseHistory = [
 	    {
 		"key": "Revenues",
-		"values": [[2011, 12850.6],[2012,13612.4],[2013, 14554.2],[2014,15252.7]]         
+		"values": []
 	    },
 	    {
 		"key": "Expenses",
-		"values": [[2011, 13840],[2012,14186.5],[2013,14184.2],[2014,14661.7]]         
+		"values": []
 	    }
    	];
 
+	var this_item = 0;
+	$interval(function(){
+//	    console.debug(this_item.toString() + ': Adding ', vu_revenues[this_item]);
+	    $scope.vuRevenueExpenseHistory[0].values.push(vu_revenues[this_item]);
+	    $scope.vuRevenueExpenseHistory[1].values.push(vu_values[this_item]);
+	    this_item++;
+//	    console.debug('History: ', $scope.vuRevenueExpenseHistory);
+	}, 200, vu_revenues.length);
+
+
+	// Data series for the Vanuatu revenue/expense multi-line chart
+*/
+
+	$scope.vuRevenueExpenseHistory = [
+	    {
+		"key": "Revenues",
+		"values": [[2010,12276],[2011,12850.6],[2012,13612.4],[2013,14554.2],[2014,15252.7]]         
+	    },
+	    {
+		"key": "Expenses",
+		"values": [[2010,13482],[2011,13840],[2012,14186.5],[2013,14184.2],[2014,14661.7]]         
+	    }
+   	];
+
+	$scope.vuDebtHistory = [
+	    {
+		"key": "External Debt",
+		"values": [[2011,10441.1],[2012,9702.2],[2013,9332.4],[2014,9249.1],[2015,11910],[2016,13639.1]]
+	    },
+	    {
+		"key": "Domestic Debt",
+		"values": [[2011,4234.9],[2012,5623.5],[2013,5929.8],[2014,5929.8],[2015,5929.8],[2016,5929.8]]
+	    }
+	];
+
+	$scope.vuDonorVsGovt = [
+	    {
+		"key" : "Donor Aid",
+		"values": [
+		    ["MoH1",831280000],["MoE1",780723000],["MoE2",702623000],["MIPU",698969000],["MoH2",567094000],
+		    ["Ports",510122000],["PMO",488499000],["TVET",333132000],["MFEM",237952000],["MoJ",237952000],
+		    ["MoH3",232442000],["ICT",187470000],["MIA",172593000],["Meteo",157204000],["VTO",156988000],
+		    ["MIA2",152289000],["MoH4",149000000],["Women",135176000],["Land",106412000],["Court",53376000],
+		    ["Water",51021000],["MoH5",38499000],["Agri",25666000],["MIA3",22873000],["Commerce",20533000],
+		    ["Agri2",19623000],["MoH6",16898000]
+		]
+	    },
+	    {
+		"key" : "Government Spending",
+		"values": [
+		    ["MoH1",792503017],["MoE1",24121329],["MoE2",661884190],["MIPU",421857218],["MoH2",139415181],
+		    ["Ports",359808218],["PMO",83942974],["TVET",28110798],["MFEM",349711457],["MoJ",28979301],
+		    ["MoH3",61055158],["ICT",280444229],["MIA",43487779],["Meteo",138811307],["VTO",23889013],
+		    ["MIA2",201170506],["MoH4",303352323],["Women",36799825],["Land",8235498],["Court",82045610],
+		    ["Water",17794398],["MoH5",49234561],["Agri",33909360],["MIA3",35316274],["Commerce",89162690],
+		    ["Agri2",58117732],["MoH6",219418294]
+		]
+	    }
+	];
+
+	$scope.vuDonorsVsGovtLabels = [
+	    "Ministry of Health, Hospital Services",
+	    "Ministry of Education, Director General's Division",
+	    "Technical and Higher Education Division",
+	    "Public Works, Development and Maintainance of Government Infrastructure",
+	    "Ministry of Health, Corporate Services",
+	    "Shipping Services, Ports Administration",
+	    "Prime Ministers Ministry Strategic Management",
+	    "TVET and Employment Opportunties",
+	    "Financial and Economic Management, Government Financial Services",
+	    "Ministry of Justice, Corporate Services",
+	    "Public Health Services",
+	    "ICT Administration",
+	    "Ministry of Internal Affairs, Administration of Regional Services",
+	    "Weather Forecasting and Monitoring",
+	    "Tourism Development", 
+	    "Internal Security and Border Control, Joint Command and Control",
+	    "Ministry of Health, Community Health Services",
+	    "Woman's Affairs",
+	    "Land Use Planning",
+	    "Supreme Court",
+	    "Rural Water Supply",
+	    "Ministry of Education, Policy and Planning Division",
+	    "Agriculture Policy and Administration",
+	    "Ministry of Internal Affairs, Corporate Services",
+	    "Executive Management Ministry of Commerce, Industry, Tourism and Ni-Vanuatu Business",
+	    "Forestry",
+	    "Medical Supplies Stock"
+	];
+
+	// return only integer values - this is needed to display years in the x axis
 	$scope.xAxisTickFormatFunction = function(){
 	    return function(d){
 		if (d % 1 === 0){
@@ -312,6 +408,14 @@ angular.module('pippDataApp.controllers.one-off-charts', ['ui.bootstrap', 'ngAni
 	    };
 	};
 
+	// For the horizontal bar chart
+	$scope.formatHBarChartTicks = function () {
+            return function(d) {
+		return int2roundM(d.toString());
+            }
+	};
+
+	// Define which palette to use. See the note above about colour choices.
 	$scope.colorFunction = function() {
 	    return function(d, i) {
     		return $scope.current_palette[i];
@@ -322,5 +426,39 @@ angular.module('pippDataApp.controllers.one-off-charts', ['ui.bootstrap', 'ngAni
             return '<h3>' + key + '</h3>' +
 		'<p>' + int2roundKMG((parseFloat(y) * 1000000).toString()) + ' VATU<br />in ' + x + '</p>';
 	};
+
+	$scope.areaChartTooltips = function(key, x, y, e, graph) {
+	    console.debug('E: ', e);
+	    
+            return '<h3>' + key + '</h3>' +
+		'<p>' + int2roundKMG((parseFloat(y) * 1000000000).toString()) + ' VATU<br />in ' + x + '</p>';
+	};
+
+	$scope.stackedLineTooltips = function(key, x, y, e, graph) {
+
+	    var label = '';
+	    var total = 0;
+
+	    if (typeof(e) != 'undefined'){
+		var search_array = $scope.vuDonorVsGovt[e.seriesIndex].values;
+
+		for (var i = 0; i < search_array.length; i++){
+		    if (search_array[i][0] == x && search_array[i][1].toLocaleString('en') + '.0' == y){
+			var other_index = e.seriesIndex === 0 ? 1 : 0;
+			total = search_array[i][1] + $scope.vuDonorVsGovt[other_index].values[i][1];
+//			console.debug("GOT IT: ", $scope.vuDonorsVsGovtLabels[i]);
+			label = $scope.vuDonorsVsGovtLabels[i];
+		    }
+		}
+	    }
+
+	    label = label != '' ? label : x;
+	    
+            return '<h3>' + label + '</h3>' +
+		'<p>' + int2roundKMG((parseFloat(y) * 1000000).toString()) + ' VATU<br /> in ' + key + '</p>' +
+		'<p>(' + int2roundKMG((parseFloat(total)).toString()) + ' vatu in total)</p>';
+
+	};
+
 
     }]);
